648b0db0ba8920e15c6a17734b477cfc6e27b46c
Luncher <gakiclin@gmail.com>
2016-11-14 09:44:13 +0800

    update redis basic

--- basic/transaction.js
+++ basic/transaction.js
@@ -1,8 +1,10 @@
 var client = require('./connect');
 var assert = require('assert');
 
+client.watch('k1');
 
 client.multi().set('k1', 'v1').get('k1').get('k1').execAsync().then(function(ret) {
+	client.unwatch();
   assert(ret[0] === 'OK');
   assert(ret[1] === 'v1');
   assert(ret[2] === 'v1');
